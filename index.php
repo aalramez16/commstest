8582877b78a6cad3bf29e748abcd525da0671321
Ali Al-Ramezi <alramzidesign@gmail.com>
2026-02-14 11:10:46 -0500

    refactor index.php, add clarifying comments

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * index.php
+ * --------------------------------------------------------------------------
+ * This is where each request lands. It initializes the app and
+ * sets up the router to handle incoming requests.
+ * 
+ * Most changes won't take place here unless they're architectural in nature.
+ * 
+ */
+
 use Slim\Factory\AppFactory;
 use DI\ContainerBuilder;
+use CommsTest\Middleware\AuthMiddleware;
+use Doctrine\ORM\EntityManager;
 
-require_once __DIR__ . '/vendor/autoload.php'; // Composer autoload
-// Normalize time zone
+/* ==========================================================================
+ * 
+ *      Initial Configurations
+ * 
+ * ========================================================================== */
+// Autoloader intelligently grabs dependencies when used.
+require_once __DIR__ . '/vendor/autoload.php';
 
+// Attach environment variables (config)
 $config = require __DIR__ . '/.config.php';
+
+// Normalize time zone
 date_default_timezone_set($config['TIME_ZONE']);
 
+/* ==========================================================================
+ * 
+ *      Dependency Injection Containers
+ * 
+ * ========================================================================== */
 // DI Containers
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions(['app.config' => $config]);
@@ -20,9 +45,36 @@ $container = $containerBuilder->build();
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-// Include routes
-(require __DIR__ . '/src/App/routes.php')($app);
-(require __DIR__ . '/src/App/middleware.php')($app);
+/* ==========================================================================
+ * 
+ *      Add Routes
+ * 
+ * ========================================================================== */
+/**
+ * core.php returns a callable that registers routes.
+ * 
+ * The callable gives a response for the root endpoint and also imports other endpoints.
+ * If an additional endpoint group gets added, it should be imported to core.php, not here.
+ */
+require __DIR__ . '/Routes/core.php'($app);
 
+/* ==========================================================================
+ * 
+ *      Add Middleware
+ * 
+ * ========================================================================== */
+// Slim Middleware Built-ins 
+$app->addRoutingMiddleware();
+$app->addBodyParsingMiddleware();
+$app->addErrorMiddleware((bool)$config['IS_DEVELOPMENT'], true, true);
+
+// Custom Middleware
+$app->add(new AuthMiddleware($container->get(EntityManager::class)));
+
+/* ==========================================================================
+ * 
+ *      Run the App
+ * 
+ * ========================================================================== */
 // Resolve the current route
 $app->run();
